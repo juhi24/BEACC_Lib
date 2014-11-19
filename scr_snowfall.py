e8fade9403809410b028786e4deb1edf29d58321
Jussi Tiira <jussi.tiira@helsinki.fi>
2014-11-19 18:54:23 +0200

    scetching

--- scr_snowfall.py
+++ scr_snowfall.py
@@ -11,8 +11,25 @@ e.autoimport_data(autoshift=False, rule='6min')
 
 for c in np.append(e.events.pluvio200.values, e.events.pluvio400.values):
     c.pluvio.shift_periods = -6
-    c.minimize_lsq()
+    #c.minimize_lsq()
     #c.plot(pip=False)
     #c.pipv.plots(save=True)
 
+p=e.events.pluvio200[2].pluvio
+d=p.data
+amount=d.acc_nrt[d.acc_nrt>0]
+a=amount.between_time('22:13', '23:30')
 
+# delta method
+v=e.events.pluvio200[2].pipv
+vel = v.good_data().vel_v
+tdelta = pd.Series(a.index.to_pydatetime(), index=a.index).diff()
+tdelta[0] = pd.datetools.timedelta(minutes=3)
+
+# groupby method
+am = d.acc_nrt.between_time('22:13', '23:30')
+group = am.astype(bool).astype(int).cumsum().shift(1).fillna(0)
+group.name = 'group'
+gdf=pd.DataFrame(group)
+vdf=pd.DataFrame(vel)
+vgrouped=pd.merge(vdf,gdf,left_index=True,right_index=True)
