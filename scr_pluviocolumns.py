6a070325032db55207c6355afaaf5c3e6bfda8e5
Jussi Tiira <jussi.tiira@helsinki.fi>
2014-11-25 13:56:39 +0200

    multiple cases handling + various minor improvements

--- scr_pluviocolumns.py
+++ scr_pluviocolumns.py
@@ -9,15 +9,25 @@ import matplotlib.pyplot as plt
 e = EventsCollection('cases/cases_of_interest.csv', '%d.%m. %H:%M')
 e.autoimport_data(autoshift=False, rule='6min')
 
-p4 = e.events.pluvio400[0].pluvio
-p4.shift_periods = -5
-p4.acc('1min').tshift(periods=5).plot(label='Bucket NRT filtered')
-raw = p4.data.bucket_nrt - p4.data.bucket_nrt[0]
-rawrt = p4.data.bucket_rt - p4.data.bucket_rt[0]
-rawacc = p4.data.acc_tot_nrt - p4.data.acc_tot_nrt[0]
-rawacc = p4.data.acc_rt.cumsum()
-raw.tshift(periods=-5,freq='1min').plot(label='Bucket NRT -5min')
-rawrt.plot(label='Bucket RT')
-rawacc.plot(label='Accumulation RT/NRT')
-rawacc.plot(label='Accumulated total NRT')
-plt.legend(loc='lower right')
+plt.close('all')
+
+for c in np.append(e.events.pluvio200.values, e.events.pluvio400.values):
+    plt.figure()
+    p4 = c.pluvio
+    tshift = -0
+    #p4.shift_periods = 0
+    acc_shift = 0.00
+    #p4.acc('1min').tshift(periods=5).plot(label='Bucket NRT filtered')
+    raw = p4.data.bucket_nrt - p4.data.bucket_rt[0]
+    rawrt = p4.data.bucket_rt - p4.data.bucket_rt[0] + acc_shift
+    rawacc = p4.data.acc_tot_nrt - p4.data.acc_tot_nrt[0]
+    rawacc = p4.data.acc_rt.cumsum()
+    sign = ''
+    if tshift>-0.1:
+        sign = '+'
+    raw.tshift(periods=tshift,freq='1min').plot(label='Bucket NRT ' + sign + str(tshift) + 'min')
+    rawrt.plot(label='Bucket RT')
+    #rawacc.plot(label='Accumulation RT/NRT')
+    #rawacc.plot(label='Accumulated total NRT')
+    plt.title(p4.name + ' ' + str(raw.index[0].date()))
+    plt.legend(loc='upper left')
