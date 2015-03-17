84babd3f5d207cf2cc666f0640ef7ac3b0bf7329
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-03-17 19:07:50 +0200

    scripting

--- scr_netcdf.py
+++ scr_netcdf.py
@@ -10,6 +10,8 @@ from netCDF4 import Dataset
 from datetime import datetime, date
 
 resultspath = '../results/netcdf'
+instr_prefix = 'PIP_004_'
+ext = '.cdf'
 
 title = 'BAECC 2014 PIP Particle Imaging Package'
 subject = 'particle size distribution, fall velocity'
@@ -25,14 +27,41 @@ cphone = '+358294150866'
 comment = 'Instrument located on the BAECC measurement field.'
 version = 'PIP_rev_1308a'
 
-date_start = date(2014, 2, 21)
+date_start = date(2014, 2, 3)
 #date_end = date(2014, 9, 12)
-date_end = date(2014, 2, 22)
+date_end = date(2014, 2, 4)
 
 for day in daterange(date_start, date_end):
     dtstr = datetime.strftime(day, '%Y%m%d')
     print(dtstr)
+    out_filepath = path.join(resultspath, instr_prefix + dtstr + ext)
     pipv_files = datafilelist(pipv_subpath % dtstr)
     dsd_files = datafilelist(dsd_subpath % dtstr)
     dsd = read.PipDSD(dsd_files)
     pipv = read.PipV(pipv_files)
+    
+    nc = Dataset(out_filepath, 'w', format='NETCDF4')
+    
+    dbinsize = nc.createDimension('bin size', 105)
+    vsize = nc.createVariable('Particle_size', 'f8', 'bin size')
+    vsize.description = 'Particle bin center of an area-equivalent diameter'
+    vsize.units = 'mm'
+    
+    dtime = nc.createDimension('time', 18)
+    vtime = nc.createVariable('Time', str, 'time')
+    vtime.description = 'Timestamp of the PIP in a minute interval'
+    vtime.units = 'UTC'
+    
+    vtime_v = nc.createVariable('Time_velocity', str, 'time')
+    vtime_v.description = 'Time stamp of the particles observed falling in a minute interval of the fall velocity data'
+    vtime_v.units = 'yyyy-mm-dd HH:MM:00'
+    
+    dvel = nc.createDimension('time_velocity')
+    vvel = nc.createVariable('Velocity', 'f4', 'time_velocity')
+    vvel.description = 'Fall velocity of the particle. The observation has more than two video frames.'
+    vvel.units = 'm/s'
+    
+    vsize_v = nc.createVariable('Particle_size_velocity', 'f4', 'time_velocity')
+    vsize_v.description = 'The area-equivalent diameter of particle of fall velocity data.'
+    vsize_v.units = 'mm'
+    nc.close()
