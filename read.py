a5fd86eeb2c3b99eae8cd30b83ad12a03665a3cc
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-06-12 14:53:41 +0300

    dsd stuff

--- read.py
+++ read.py
@@ -16,6 +16,7 @@ from scipy import stats
 from scipy.optimize import fmin, minimize
 from fit import *
 from pytmatrix import tmatrix_aux as aux
+from pytmatrix import psd
 
 RESULTS_DIR = '../results'
 CACHE_DIR = 'cache'
@@ -1071,12 +1072,16 @@ TAU = 2*np.pi
 
 def ar(d):
     return 1/aux.dsr_thurai_2007(d)
+    
+def drop_zeros_rows(df):
+    return df[(df.T != 0).any()]
 
 class DSD(InstrumentData):
     def __init__(self, data=None, binwidth=None, bin_v=None):
-        self.data = data
+        self.data = data.resample('1min').fillna(0)
         self.binwidth = binwidth
         self.bin_v = pd.Series(bin_v, index=data.columns.values)
+        self.drop_empty = True
 
     def plot(self, img=True, **kwargs):
         """Plot particle size distribution over time."""
@@ -1090,57 +1095,75 @@ class DSD(InstrumentData):
         plt.title('DSD')
         plt.xlabel('time (UTC) BROKEN')
         plt.ylabel('D (mm) BROKEN')
-    
+
     def binwidth_df(self):
         return pd.DataFrame(data=self.binwidth, index=self.bin_cen()).T
-        
-    def good_data(self):
-        return self.data
-        
+
+    def good_data(self, drop_empty=True):
+        data = self.data
+        if self.drop_empty and drop_empty:
+            data = drop_zeros_rows(data)
+        return data
+
     def intensity(self):
         return self.sum_over_d(self.rr)
 
     def v(self, d):
         return self.bin_v[self.bin_select(d)]
-        
+
     def n(self, d):
         return self.good_data()[d]
-    
+
     def bin_cen(self):
         return self.good_data().columns.values
-        
+
     def bin_lower(self):
         return self.bin_cen() - 0.5*self.binwidth
-        
+
     def bin_upper(self):
         return self.bin_lower() + self.binwidth
-        
+
     def bin_edge(self):
         return np.append(self.bin_lower(), self.bin_upper()[-1])
-    
+
     def bin_edge_df(self):
         edge = pd.DataFrame(data=[self.bin_lower(), self.bin_upper()]).T
         edge.columns = ['lower', 'upper']
         edge.index = self.bin_cen()
         return edge
-        
+
     def bin_select(self, d):
         for i, edge in self.bin_edge_df().iterrows():
             if d > edge.lower and d < edge.upper:
                 return edge.name
         return
-        
+
     def series_zeros(self):
-        s = self.data[self.data.columns[0]]*0
+        s = self.good_data()[self.good_data().columns[0]]*0
         s.name = 'series'
         return s
-        
+
     def sum_over_d(self, func, **kwargs):
         dD = self.binwidth
         result = self.series_zeros()
         for d in self.bin_cen():
             result = result.add(func(d, **kwargs)*dD[d], fill_value=0)
         return result
-        
+
     def rr(self, d):
         return 3.6e-3*TAU/12*(ar(d)*d)**2*1/ar(d)*d*self.v(d)*self.n(d)
+
+    def to_tm(self, data=None):
+        if data is None:
+            data = self.good_data().mean()
+        return psd.BinnedPSD(bin_edges=self.bin_edge(),
+                             bin_psd=data.values)
+
+    def to_tm_series(self, resample=None):
+        if resample is None:
+            data = self.good_data()
+        else:
+            data = self.good_data(drop_empty=False).resample(resample, how=np.mean,
+                                                             closed='right',
+                                                             label='right')
+        return data.apply(self.to_tm, axis=1)
--- scr_tartu_tmatrix.py
+++ scr_tartu_tmatrix.py
@@ -7,12 +7,19 @@ import read
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 from os import path
+from pytmatrix import tmatrix, radar
+from pytmatrix import refractive as ref
+from pytmatrix import orientation as ori
+from pytmatrix import tmatrix_aux as aux
+from pytmatrix import psd
 
 datapath = '../DATA/tartu2015'
 
 dT = 60.0 # disdrometer sampling time, seconds
+
+def db(data):
+    return 10*np.log10(data)
 
 # Taken from http://www.helsinki.fi/~mleskine/sateet/RD69.html
 dsd_D = np.array([0.3671, 0.4643, 0.5611, 0.6667, 0.7822, 0.9245, 1.1275, 
@@ -34,4 +41,46 @@ dsd = read.DSD(data=dsd_data, binwidth=dsd_dD, bin_v=dsd_v)
 sep1 = dsd.between_datetime('2012-09-01 08:00', '2012-09-2 00:00')
 
 #sep1.plot(img=False)
+drops = tmatrix.Scatterer(wavelength=aux.wl_C,m=ref.m_w_10C[aux.wl_C])
+drops.Kw_sqr = aux.K_w_sqr[aux.wl_C]
+drops.or_pdf = ori.gaussian_pdf(std=7.0)
+drops.orient = ori.orient_averaged_fixed
+drops.psd_integrator = psd.PSDIntegrator()
+drops.psd_integrator.D_max = 10.0
+drops.psd_integrator.axis_ratio_func = read.ar
 
+back = aux.geom_horiz_back
+forw = aux.geom_horiz_forw
+drops.psd_integrator.geometries = (back,forw)
+
+drops.psd_integrator.init_scatter_table(drops)
+
+psds = sep1.to_tm_series(resample=None)
+
+drops.set_geometry(back)
+
+zh = []
+zv = []
+zdr = []
+rho_hv = []
+ldr = []
+
+for tm_psd in psds:
+    drops.psd = tm_psd
+    zh.append(db(radar.refl(drops)))
+    zv.append(db(radar.refl(drops,False)))
+    zdr.append(db(radar.Zdr(drops)))
+    rho_hv.append(radar.rho_hv(drops))
+    ldr.append(db(radar.ldr(drops)))
+
+d = {'R':sep1.intensity(), 'Zh': zh, 'Zv': zv, 'Zdr': zdr, 'rho_hv': rho_hv,
+     'LDR': ldr}
+params = pd.DataFrame(data=d, index=psds.index)
+
+#print('Zh\t%s\t[dBZ]' % db(radar.refl(drops)))
+#print('Zv\t%s\t[dBZ]' % db(radar.refl(drops,False)))
+#print('Zdr\t%s\t[dBZ]' % db(radar.Zdr(drops)))
+#print('rho_hv\t%s' % radar.rho_hv(drops))
+#print 'del_hv\t',radar.delta_hv(drops), '\t[rad]'
+#print 'ldr\t',radar.ldr(drops) 
+#print 'LDR\t',10*np.log10(radar.ldr(drops)) ,'\t[dBZ]'
