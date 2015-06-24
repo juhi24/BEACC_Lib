fixed radar averaging over time interval

read.py
@@ -249,6 +249,7 @@ class Radar(InstrumentData):
         if self.data.empty and filenames:
             self.name = (os.path.basename(os.path.dirname(self.filenames[0])))
             for filename in filenames:
+                print(filename)
                 radardata = io.netcdf.netcdf_file(filename)
                 radarvariables = radardata.variables
                 if filename.endswith('.nc'):
@@ -256,9 +257,9 @@ class Radar(InstrumentData):
                         range_idx = 1
                     if 'KaSACR' in radardata.title.decode():
                         range_idx = 0
-                    reflectivity = radarvariables['reflectivity'].data[:,range_idx]*radarvariables['reflectivity'].scale_factor  + radarvariables['reflectivity'].add_offset
+                    refl = radarvariables['reflectivity']
+                    reflectivity = 10.0**(0.1*(refl.data[:,range_idx]*refl.scale_factor + refl.add_offset))
                     basetime = datetime.datetime.strptime(radarvariables['time'].units.decode(),'seconds since %Y-%m-%dT%H:%M:%SZ')
-                    #delta = (np.round(radarvariables['time'].data/60.0)*60.0).astype(int)
                     delta = radarvariables['time'].data
                     deltatime = pd.to_timedelta(delta,unit='s')
                     time = basetime + deltatime # + time_lag
@@ -271,45 +272,30 @@ class Radar(InstrumentData):
                 elif filename.endswith('.cdf'):
                     if 'reflectivity_copol' in radarvariables.keys():
                         range_idx = 10
-                        reflectivity = radarvariables['reflectivity_copol'].data[:,range_idx].byteswap().newbyteorder()
+                        reflectivity = 10.0**(0.1*radarvariables['reflectivity_copol'].data[:,range_idx].byteswap().newbyteorder())
                     elif 'reflectivity' in radarvariables.keys():
                         range_idx = 6
-                        ref1 = radarvariables['reflectivity'].data[:,range_idx]
-                        ref2 = radarvariables['reflectivity'].data[:,range_idx+1]
-                        reflectivity = 10.0*np.log10(0.5*(10.0**(0.1*ref1)+10.0**(0.1*ref2)))
+                        ref1 = 10.0**(0.1*radarvariables['reflectivity'].data[:,range_idx])
+                        ref2 = 10.0**(0.1*radarvariables['reflectivity'].data[:,range_idx+1])
+                        reflectivity = 0.5*(ref1+ref2)
                     basetime = datetime.datetime.strptime(radarvariables['time'].units.decode(),'seconds since %Y-%m-%d %H:%M:%S 0:00')
                     delta = radarvariables['time'].data
-                    #delta = (np.round(radarvariables['time'].data/60.0)*60.0).astype(int)
                     deltatime = pd.to_timedelta(delta,unit='s')
                     time = basetime + deltatime # + time_lag
                     timetrunc = pd.DatetimeIndex(((time.astype(np.int64) // ns1min + 1 ) * ns1min))
                     self.data = pd.DataFrame(reflectivity,index=timetrunc,
                                              columns=['reflectivity'],dtype=np.float64)
         self.finish_init(dt_start, dt_end)
-        
-#    def grouped(self, varinterval=True, rule=None): # TODO fix grouping
-#        if rule is None:
-#            rule = self.rule
-#        if varinterval:
-#            data = self.good_data()
-#            grpd_data = pd.merge(data, rule, left_index=True, right_index=True)
-#            return grpd_data.groupby('group')
-#        return self.good_data().groupby(pd.Grouper(freq=rule, closed='right', label='right'))
-    
-    def Z(self, rule=None, varinterval=True):
+         
+    def z(self, rule=None, varinterval=True):
         """Reflectivity time series"""
-        for name, group in self.grouped(rule=rule, varinterval=varinterval):
-            print('NAME')
-            print(name)
-            print('GROUP')
-            print(group)
-        return self.grouped(rule=rule, varinterval=varinterval)
-        
-    def Zavg(Z):
-        """Return average of reflectivity"""
-        return 10.0*np.log10( (10.0**(0.1*Z)).mean() )
-
-                    
+        grp = self.grouped(rule=rule, varinterval=varinterval)
+        z = grp.mean()
+        zs = z[z.columns[0]]
+        zs.name = self. name + ' reflectivity'
+        zs.index.name = 'datetime'
+        return zs
+                           
 class Pluvio(InstrumentData, PrecipMeasurer):
     """Pluviometer data handling"""
     def __init__(self, filenames, dt_start=None, dt_end=None, **kwargs):
@@ -763,7 +749,6 @@ class PipV(InstrumentData):
         if rule is None:
             rule = self.rule
         if self.fits.empty:
-            print(rule)
             self.find_fits(rule, fit=emptyfit, varinterval=varinterval)
         elif not varinterval:
             if pd.datetools.to_offset(rule) != self.fits.index.freq:

snowfall.py
@@ -150,14 +150,14 @@ class EventsCollection(MultiSeries):
                                                autobias=autobias))
         self.events[data.pluvio.name] = cases
 
-    def autoimport_data(self, datafile=[h5path],
-                        autoshift=False, autobias=False, **casekwargs):
+    def autoimport_data(self, datafile=[h5path], autoshift=False, 
+                        autobias=False, radar=False, **casekwargs):
         """Import data from a hdf file."""
         timemargin = pd.datetools.timedelta(hours=3)
         dt_start = self.events.iloc[0].start - timemargin
         dt_end = self.events.iloc[-1].end + timemargin
         data = Case.from_hdf(dt_start, dt_end, autoshift=False,
-                             filenames=datafile, **casekwargs)
+                             filenames=datafile, radar=radar, **casekwargs)
         for d in data:
             if d is not None:
                 self.add_data(d, autoshift=autoshift, autobias=autobias)
@@ -179,7 +179,6 @@ class Case(read.PrecipMeasurer, read.Cacher, MultiSeries):
         self.dsd = dsd
         self.pipv = pipv
         self.pluvio = pluvio
-#       if xsacr is not None:
         self.xsacr = xsacr
         self.kasacr = kasacr
         self.kazr = kazr
@@ -566,9 +565,16 @@ class Case(read.PrecipMeasurer, read.Cacher, MultiSeries):
         plt.legend()
         return ax
 
-    def Z(self):
+    def z(self, radarname='XSACR'):
         """Radar reflectivity wrapper"""
-        return self.xsacr.Z(varinterval=self.varinterval, rule=self.rule)
+        if radarname == 'XSACR':
+            return self.xsacr.z(varinterval=self.varinterval, rule=self.rule)
+        elif radarname == 'KASACR':
+            return self.kasacr.z(varinterval=self.varinterval, rule=self.rule)
+        elif radarname == 'KAZR':
+            return self.kazr.z(varinterval=self.varinterval, rule=self.rule)
+        elif radarname == 'MWACR':
+            return self.mwacr.z(varinterval=self.varinterval, rule=self.rule)
         
     def Z_rayleigh_Xband(self, pluvio_filter=True, pip_filter=False):
         """Use rayleigh formula and maxwell-garnett EMA to compute radar reflectivity Z"""
@@ -622,7 +628,7 @@ class Case(read.PrecipMeasurer, read.Cacher, MultiSeries):
                 Z = 10.0*np.log10(radar.refl(flake))
             else:
                 Z = np.nan
-            print(ref,item[0],Z)
+            #print(ref,item[0],Z)
             Zserie.loc[item[0]] = Z
         Zserie.name = name
         return Zserie

src_beta.py
@@ -40,7 +40,7 @@ import matplotlib.pyplot as plt
 ####batch_create_hdf(datadir='../DATA', outname='new_winter.h5',dtstr='20150201') #NODATA
 #batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140131')
 #batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140201')
-#batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140212')
+batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140212')
 #batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140215')
 #batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140216')
 #batch_create_hdf(datadir='../DATA', outname='baecc3.h5',dtstr='20140221')
@@ -57,9 +57,9 @@ dtformat_print = '%y%m%d%H%M'
 folder = '/home/dori/SnowCases_BAEC/DensityJussi/test/'
 #RadarVP.to_csv(folder + 'radar_data.csv')
 
-e = EventsCollection('cases/test2.csv', dtformat_default_year)
+e = EventsCollection('cases/cases_of_interest_radar.csv', dtformat_default_year)
 e.autoimport_data(autoshift=False, autobias=False, rule='5min',
-                  varinterval=True, datafile=['../DATA/baecc3.h5'])
+                  varinterval=True, radar=True, datafile=['../DATA/baecc3.h5'])
 
 def func_all_beta(mu,delta,deltaZ):
     def func_beta(beta):
@@ -105,11 +105,23 @@ for c in e.events.pluvio200.values:
     c.pluvio.shift_periods = -6
     basename = folder + datetime.strftime(c.pluvio.dt_start().to_datetime(),'%Y%m%d%H')
     print(datetime.strftime(c.pluvio.dt_start().to_datetime(),'%Y%m%d%H'))
-    c.pluvio.n_combined_intervals = 2
-    #c.v(0.3)
-    z = c.Z()
-    #z.plot()
-    #plt.savefig('xsacr_avg')
+    c.pluvio.n_combined_intervals = 1
+    zx = 10.0*np.log10(c.z('XSACR'))
+    zk = 10.0*np.log10(c.z('KASACR'))
+    zkz = 10.0*np.log10(c.z('KAZR'))
+    zmw = 10.0*np.log10(c.z('MWACR'))
+    plt.figure()
+    zx.plot(label='xsacr')
+    #zk.plot(label='kasacr')
+    #zkz.plot(label='kazr')
+    #zmw.plot(label='mwacr')
+    zxtm = c.tmatrix(wl=30.87)
+    zxray = c.Z_rayleigh_Xband()
+    zxtm.plot(label='Xtm')
+    zxray.plot(label='Xray')
+    plt.legend(loc=0)
+    plt.savefig(basename + 'radar_avg.png')
+    plt.close()
     #depth = c.amount(params=[100],simple=True)
     #depth.to_csv(basename + 'depth_' + c.pluvio.name + '.csv')
     #print(depth.sum())