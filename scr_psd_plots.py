322c0fe0c38c726402d6d89214316bdd45b8d97d
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-09-01 18:17:22 +0300

    psd plot scripting

--- scr_psd_plots.py
+++ scr_psd_plots.py
@@ -10,10 +10,10 @@ from os import path
 dtformat_default = '%d.%m. %H:%M'
 dtformat_snex = '%Y %d %B %H UTC'
 
-e = EventsCollection('cases/pip2015test.csv', dtformat_snex)
+e = EventsCollection('cases/pip2015.csv', dtformat_snex)
 e.autoimport_data(autoshift=False, autobias=False, rule='6min', varinterval=True)
 
-plt.close('all')
+#plt.close('all')
 #plt.ioff()
 plt.ion()
 
@@ -24,25 +24,39 @@ for c in np.append(e.events.pluvio200.values, e.events.pluvio400.values):
 comb200 = e.events.pluvio200.sum()
 comb400 = e.events.pluvio400.sum()
 del(e)
+
+limslist = limitslist((0, 150, 300, 800))
+n_ranges = len(limslist)
+separate = False
+
+basepath = '../results/pip2015/paper/psd'
+
 for c in (comb200, comb400):
     d0 = c.d_0()
     nt = c.n_t()
     n = c.intervalled(c.instr['dsd'].n_all)
     y = n*d0/nt
     d = y*0+y.columns.values
-    x = d/d0
-    for rhorange in limitslist((0, 150, 300, 800)):
+    x = d#/d0
+    if not separate:
+        fig, axarr = plt.subplots(1, n_ranges, dpi=100, sharex=True, sharey=True,
+                                  tight_layout=True, figsize=(n_ranges*6, 6))
+    for i, rhorange in enumerate(limslist):
+        if separate:
+            fig, ax = plt.subplots(dpi=100)
+        else:
+            ax = axarr[i]
         rhomin = rhorange[0]
         rhomax = rhorange[1]
         xpart = c.data_in_density_range(x, rhomin, rhomax)
-        ypart = c.data_in_density_range(y, rhorange[0], rhorange[1])
+        ypart = c.data_in_density_range(y, rhomin, rhomax)
         if xpart.empty or ypart.empty:
+            ax.semilogy()
             continue
-        plt.figure(dpi=100)
-        plt.semilogy(xpart, ypart, marker='o', linestyle='None', color='black')
-        plt.axis([0, 5, 10e-5, 10])
-        plt.ylabel('$N_D D_0 N_T^{-1}$')
-        plt.xlabel('$D D_0^{-1}$')
-        plt.title('$%s < \\rho < %s$' % (rhomin, rhomax))
-        plt.tight_layout()
-    plt.title('$\\rho > ' + str(rhomin) + '$')
+        ax.semilogy(xpart, ypart, marker='o', linestyle='None', color='black')
+        ax.set_ylabel('$N_D D_0 N_T^{-1}$')
+        ax.set_xlabel('$D D_0^{-1}$')
+        ax.set_title('$%s < \\rho < %s$' % (rhomin, rhomax))
+        ax.grid(True)
+    plt.axis([0, 5, 10e-5, 10])
+    ax.set_title('$\\rho > ' + str(rhomin) + '$')
