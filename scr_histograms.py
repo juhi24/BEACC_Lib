23a6821b2fa5340111e66cd715905ce236f73a8b
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-09-24 16:13:19 +0300

    save before merge

--- scr_histograms.py
+++ scr_histograms.py
@@ -62,24 +62,27 @@ for i, c in enumerate(e.events.pluvio400.values):
           fontdict={'verticalalignment': 'top', 'fontsize': 10})
 
 comb400 = e.events.pluvio400.sum()
+comb200 = e.events.pluvio200.sum()
 limslist = limitslist((0, 150, 300, 800))
 n_ranges = len(limslist)
-fdd, axarrdd = subplots(n_ranges)
-fmd, axarrmd = subplots(n_ranges)
-fnd, axarrnd = subplots(n_ranges)
-data400 = read.merge_multiseries(comb400.d_0(), comb400.mu(), comb400.n_w())
 
-for i, lims in enumerate(limslist):
-    data = comb400.data_in_density_range(data400, *lims)
-    limitstr = '$%s < \\rho < %s$' % (lims[0], lims[1])
-    plots(data, axarrdd[i], axarrmd[i], axarrnd[i], title=limitstr, y=0.9, fontdict={'verticalalignment': 'top'})
-
-for ax in (axarrdd[-1], axarrmd[-1], axarrnd[-1]):
-    ax.set_title('$\\rho > %s$' % lims[0], y=0.9, fontdict={'verticalalignment': 'top'})
-
-for f in (fd, fm, fn, fdd, fmd, fnd):
-    remove_gaps(f)
-
-for axarr in (axarrd, axarrm, axarrn, axarrdd, axarrmd, axarrnd):
-    for ax in axarr[1:]:
-        sns.despine(ax=ax, top=True, left=False, right=False, bottom=False)
+for c in (comb200, comb400)
+    fdd, axarrdd = subplots(n_ranges)
+    fmd, axarrmd = subplots(n_ranges)
+    fnd, axarrnd = subplots(n_ranges)
+    data = read.merge_multiseries(c.d_0(), c.mu(), c.n_w())
+    
+    for i, lims in enumerate(limslist):
+        dat = c.data_in_density_range(data, *lims)
+        limitstr = '$%s < \\rho < %s$' % (lims[0], lims[1])
+        plots(dat, axarrdd[i], axarrmd[i], axarrnd[i], title=limitstr, y=0.9, fontdict={'verticalalignment': 'top'})
+    
+    for ax in (axarrdd[-1], axarrmd[-1], axarrnd[-1]):
+        ax.set_title('$\\rho > %s$' % lims[0], y=0.9, fontdict={'verticalalignment': 'top'})
+    
+    for f in (fd, fm, fn, fdd, fmd, fnd):
+        remove_gaps(f)
+    
+    for axarr in (axarrd, axarrm, axarrn, axarrdd, axarrmd, axarrnd):
+        for ax in axarr[1:]:
+            sns.despine(ax=ax, top=True, left=False, right=False, bottom=False)
--- scr_psd_plots.py
+++ scr_psd_plots.py
@@ -6,6 +6,7 @@ from snowfall import *
 import numpy as np
 import matplotlib.pyplot as plt
 from os import path
+import seaborn as sns
 
 dtformat_default = '%d.%m. %H:%M'
 dtformat_snex = '%Y %d %B %H UTC'
@@ -13,7 +14,7 @@ dtformat_snex = '%Y %d %B %H UTC'
 e = EventsCollection('cases/pip2015.csv', dtformat_snex)
 e.autoimport_data(autoshift=False, autobias=False, rule='6min', varinterval=True)
 
-#plt.close('all')
+plt.close('all')
 #plt.ioff()
 plt.ion()
 
@@ -33,32 +34,45 @@ basepath = '../results/pip2015/paper/psd'
 
 for c in (comb200, comb400):
     d0 = c.d_0()
-    nt = c.n_t()
-    nw = c.n_w()
-    n = c.intervalled(c.instr['dsd'].psd)
-    y = n/nw
-    d = y*0+y.columns.values
-    xd = {'D':d, '$D D_0^{-1}$':d/d0}
-    for xlabel, x in xd.items():
-        if not separate:
-            fig, axarr = plt.subplots(1, n_ranges, dpi=100, sharex=True, sharey=True,
-                                      tight_layout=True, figsize=(n_ranges*6, 6))
-        for i, rhorange in enumerate(limslist):
-            if separate:
-                fig, ax = plt.subplots(dpi=100)
-            else:
-                ax = axarr[i]
-            rhomin = rhorange[0]
-            rhomax = rhorange[1]
-            xpart = c.data_in_density_range(x, rhomin, rhomax)
-            ypart = c.data_in_density_range(y, rhomin, rhomax)
-            if xpart.empty or ypart.empty:
-                ax.semilogy()
-                continue
-            ax.semilogy(xpart, ypart, marker='o', linestyle='None', color='black')
-            ax.set_ylabel('$N_D N_w^{-1}$')
-            ax.set_xlabel(xlabel)
-            ax.set_title('$%s < \\rho < %s$' % (rhomin, rhomax))
-            ax.grid(True)
-        plt.axis([0, 5, 10e-5, 1000])
-        ax.set_title('$\\rho > ' + str(rhomin) + '$')
+    nw = np.log10(c.n_w())
+    df = read.merge_multiseries(d0, nw)
+    data = df.dropna()
+    f, ax = plt.subplots(dpi=120, tight_layout=True)
+    df.plot(kind='hexbin', x='D_0', y='N_w', logy=False, ax=ax)
+    sns.jointplot(x='D_0', y='N_w', data=data, kind='kde', stat_func=None,
+                  joint_kws={'kde_kws':{'bw':0.001}})
+    sns.jointplot(x='D_0', y='N_w', data=data, kind='hex', stat_func=None)
+    sns.jointplot(x='D_0', y='N_w', data=data, kind='scatter', stat_func=None)
+
+def psds_in_rho_range():
+    for c in (comb200, comb400):
+        d0 = c.d_0()
+        nt = c.n_t()
+        nw = c.n_w()
+        n = c.intervalled(c.instr['dsd'].psd)
+        y = n/nw
+        d = y*0+y.columns.values
+        xd = {'D':d, '$D D_0^{-1}$':d/d0}
+        for xlabel, x in xd.items():
+            if not separate:
+                fig, axarr = plt.subplots(1, n_ranges, dpi=100, sharex=True, sharey=True,
+                                          tight_layout=True, figsize=(n_ranges*6, 6))
+            for i, rhorange in enumerate(limslist):
+                if separate:
+                    fig, ax = plt.subplots(dpi=100)
+                else:
+                    ax = axarr[i]
+                rhomin = rhorange[0]
+                rhomax = rhorange[1]
+                xpart = c.data_in_density_range(x, rhomin, rhomax)
+                ypart = c.data_in_density_range(y, rhomin, rhomax)
+                if xpart.empty or ypart.empty:
+                    ax.semilogy()
+                    continue
+                ax.semilogy(xpart, ypart, marker='o', linestyle='None', color='black')
+                ax.set_ylabel('$N_D N_w^{-1}$')
+                ax.set_xlabel(xlabel)
+                ax.set_title('$%s < \\rho < %s$' % (rhomin, rhomax))
+                ax.grid(True)
+            plt.axis([0, 5, 10e-5, 1000])
+            ax.set_title('$\\rho > ' + str(rhomin) + '$')
