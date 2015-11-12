35c74371f123642e46a9301b1dfd2fde9dcc0a4f
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-11-12 18:53:18 +0200

    scripting: add lwp data

--- scr_case_overview.py
+++ scr_case_overview.py
@@ -16,13 +16,21 @@ if debug:
 else:
     e = pip2015events()
 
+cdf_path = '/home/jussitii/DATA/arm/tmpmwrlosM1.b1.20140201.000113.cdf'
+vap = read.cdf_to_series(cdf_path, 'vap')
+
 case = e.events.paper[0]
 data = case.summary()
+vap = vap[data.index[0]:data.index[-1]].copy()
 
-params = ['intensity', 'density', 'D_0', 'N_0', 'N_w']
+params = ['intensity', 'density', 'D_0', 'N_0', 'N_w', '']
 axarr = data.loc[:, params].plot(subplots=True, drawstyle='steps')
-
 axdict = dict(zip(params, axarr))
+fig = axarr[0].get_figure()
+vap.plot(ax=axarr[-1], label='lwp')
+
 
 for param in ['N_0', 'N_w']:
     axdict[param].set_yscale('log')
+
+plt.tight_layout()
