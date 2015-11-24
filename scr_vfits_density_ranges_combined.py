eddad5df5acf9535b5df37ab88a9697a55d1c272
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-11-24 12:29:58 +0200

    scripting: d0-filtered plot

--- scr_vfits_density_ranges_combined.py
+++ scr_vfits_density_ranges_combined.py
@@ -11,9 +11,10 @@ import fit
 
 from scr_snowfall import pip2015events, test_events
 
-debug = True
+debug = False
 unfiltered = False
 tld = '.eps'
+rholimits = (0, 150, 300, 800)
 
 if debug:
     e = test_events()
@@ -26,20 +27,25 @@ plt.ion()
 comb = e.events.paper.sum()
 del(e)
 
-kws = {}
+kws = {'separate': True,
+       'rholimits': rholimits,
+       'source_style': 'kde',
+       'unfiltered': True}
 fitargs = {'force_flip': False,
            'try_flip': False,
-           'fitclass': fit.PolFit,
-           'frac': 0.1}
+           'fitclass': fit.PolFit}
 if unfiltered:
     fitargs['filter_outliers'] = False
-fig, axarr = comb.plot_vfits_in_density_ranges(separate=True,
-                                               rholimits=(0, 150, 300, 800),
-                                               source_style='kde',
-                                               fitargs=fitargs,
-                                               unfiltered=True,
-                                               **kws)
-plt.axis((0.25,2.8,0.5,1.8))
+fig, axarr = comb.plot_vfits_in_density_ranges(fitargs=fitargs, **kws)
+merger = comb.d_0()
+data = comb.instr['pipv'].good_data()
+data_grouped = comb.group(data, merger)
+data_fltr = data_grouped[data_grouped['D_0'] > 0.63]
+fitargs['data'] = data_fltr
+fig_fltr, axarr_fltr = comb.plot_vfits_in_density_ranges(fitargs=fitargs, **kws)
+bnds = (0.25,2.8,0.5,1.8)
+axarr[0].axis(bnds)
+axarr_fltr[0].axis(bnds)
 
 savepath = '../results/pip2015/vfits_density_ranges'
 if debug:
@@ -48,4 +54,5 @@ read.ensure_dir(savepath)
 fname = 'combined'
 if unfiltered:
     fname += '_unfiltered'
-plt.savefig(path.join(savepath, fname + tld))
+fig.savefig(path.join(savepath, fname + tld))
+fig_fltr.savefig(path.join(savepath, fname + '_d0fltr'))
