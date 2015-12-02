6325638b9fe6a74643ed38726cdcc6cb675ee2e4
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-12-02 21:58:10 +0200

    script refactor

--- scr_case_overview.py
+++ scr_case_overview.py
@@ -11,7 +11,7 @@ from scr_snowfall import pip2015events, test_events
 
 plt.close('all')
 plt.ion()
-debug = False
+debug = True
 include_vap = False
 savedir = '../results/pip2015/case_overview'
 
@@ -30,32 +30,29 @@ def gray_out(data, axdict, fltr_label='d0_fltr', labels=['D_0', 'density']):
             ax.axvspan(row.start, t_end, edgecolor='none', facecolor='0.8',
                        alpha=0.8)
 
-if include_vap:
-    df_path = '/home/jussitii/DATA/arm/tmpmwrlosM1.b1.20140201.000113.cdf'
-    vap = read.cdf_to_series(cdf_path, 'vap')
-
-for case in e.events.paper.values:
-    data = case.summary()
-    if include_vap:
-        vap = vap[data.index[0]:data.index[-1]].copy()
-    
-    params = ['intensity', 'density', 'D_0', 'N_w']
-    start_time = case.instr['pluvio'].start_time()
+def plot_overview(data, params=['intensity', 'density', 'D_0', 'N_w'],
+                  axlist=None):
     data.density[data.density>800] = np.nan
-    if include_vap:
-        params.append('')
-    axarr = data.loc[:, params].plot(figsize=(5,7), subplots=True,
-                                     drawstyle='steps')
-    axdict = dict(zip(params, axarr))
-    fig = axarr[0].get_figure()
-    if include_vap:
-        vap.plot(ax=axarr[-1], label='LWP')
+    if axlist is None:
+        axlist = data.loc[:, params].plot(figsize=(5,7), subplots=True,
+                                         drawstyle='steps')
+    else:
+        for i, param in enumerate(params):
+            ax = axlist[i]
+            data[param].plot(ax=ax, drawstyle='steps')
+    axdict = dict(zip(params, axlist))
+    fig = axlist[0].get_figure()
     #axdict['density'].axis((None, None, 0, 600))
     for param in ['N_w']:
         axdict[param].set_yscale('log')
     data = read.merge_series(data, case.instr['pluvio'].start_time())
     data['d0_fltr'] = data.D_0 < 0.63
     gray_out(data, axdict)
+    return fig, axlist
+
+for case in e.events.paper.values:
+    data = case.summary()
+    fig, axlist = plot_overview(data)
     plt.legend()
     plt.tight_layout()
     fig.savefig(path.join(savedir, case.dtstr('%Y%m%d') + '.eps'), dpi=150)
