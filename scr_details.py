e2df6cc3b177697e9e37cc6761fcd8d90d6d61c6
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-05-12 09:41:15 +0300

    scripting

--- scr_details.py
+++ scr_details.py
@@ -6,6 +6,7 @@ Details and statistics script.
 
 from scr_snowfall import param_table
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 plt.ion()
 QSTR_FILTERED = 'density>599 | count<801 | b<0 | density!=density'
@@ -17,3 +18,8 @@ def param_table_unfiltered():
 def param_table_filtered_rows():
     qstr = QSTR_FILTERED
     return param_table(query_str=qstr)
+
+def plot_intensity_hist(data, bins=18):
+    ax = data.intensity.hist(bins=bins)
+    ax.set_ylabel('frequency')
+    ax.set_xlabel('LWE')
