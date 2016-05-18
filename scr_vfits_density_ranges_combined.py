f6a594b99bba85e9c3e31a68d6b73636d34b7ee0
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-05-18 11:05:00 +0300

    rewrite script

--- scr_vfits_density_ranges_combined.py
+++ scr_vfits_density_ranges_combined.py
@@ -15,23 +15,13 @@ from scr_snowfall import pip2015events, test_events, rholimits, param_table
 
 debug = False
 unfiltered = False
+legacy = False
 tld = '.eps'
-bnds = (0.35,3.0,0.5,1.8)
+bnds = (0.25,3.0,0.5,1.8)
 
-def vel_fit(data, rhomin=None):
-    if rhomin is not None:
-        data = data.query('rhomin=={0}'.format(rhomin))
-    d = np.concatenate([vfit.x_unfiltered for vfit in data.polfit])
-    v = np.concatenate([vfit.y_unfiltered for vfit in data.polfit])
-    return d, v
 
-def vfits_debug(comb):
-    fitargs = {'force_flip': False,
-               'try_flip': False,
-               'fitclass': fit.PolFit}
-    rholimits=(0, 100, 200, 800)
-    limslist = sf.limitslist(rholimits)
-    return comb.vfits_density_range(limslist, **fitargs)
+def dict2tuple(di):
+    return tuple(di[key] for key in sorted(di))
 
 if debug:
     e = test_events()
@@ -41,35 +31,29 @@ else:
 #plt.close('all')
 plt.ioff()
 
-data = param_table(e=e, debug=debug, rho_limits=rholimits)
-data.index = data.index.droplevel()
-vdata = read.merge_series(e.pluv_grouper(), e.vel_data())
-table = pd.merge(vdata, pd.DataFrame(data.rhomin), left_on='group', right_index=True)
-
-comb = e.events.paper.sum()
-del(e)
-gc.collect()
-
 hextent = np.array(bnds)+(-0.1,0.1,-0.1,0.1)
 kws = {'separate': True,
-       'rholimits': rholimits,
        'source_style': 'hex',
        'source_kws': {'gridsize': 26, 'extent': hextent},
        'unfiltered': True}
 fitargs = {'force_flip': False,
            'try_flip': False,
            'fitclass': fit.PolFit}
-if unfiltered:
-    fitargs['filter_outliers'] = False
-fig, axarr = comb.plot_vfits_in_density_ranges(fitargs=fitargs, **kws)
-#merger = comb.d_0()
-#data = comb.instr['pipv'].good_data()
-#data_grouped = comb.group(data, merger)
-#data_fltr = data_grouped[data_grouped['D_0'] > 0.63]
-#fitargs['data'] = data_fltr
-#fig_fltr, axarr_fltr = comb.plot_vfits_in_density_ranges(fitargs=fitargs, **kws)
+
+limslist=sf.limitslist(rholimits)
+data = param_table(e=e, debug=debug, rho_limits=rholimits)
+data.index = data.index.droplevel()
+vdata = read.merge_series(e.pluv_grouper(), e.vel_data())
+vtable = pd.merge(vdata, pd.DataFrame(data.rhomin), left_on='group', right_index=True)
+# TODO: check duplicates
+vfits = dict()
+for rhomin in rholimits[:-1]:
+    pipv = read.PipV(data=vtable.query('rhomin=={0}'.format(rhomin)))
+    vfits[rhomin], std, etc = pipv.pickler('v-d_rho_range', pipv.find_fit, **fitargs)
+fig, axarr = sf.plot_vfits_rho_intervals(dict2tuple(vfits), limslist, **kws)
 axarr[0].axis(bnds)
-#axarr_fltr[0].axis(bnds)
+for ax in axarr:
+    ax.tick_params(direction='out', top=False)
 
 resultsdir = '../results/pip2015'
 savepath = path.join(resultsdir, 'vfits_density_ranges')
