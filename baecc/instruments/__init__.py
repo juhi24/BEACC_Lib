5ae57a1d7d96de5160aaaa6338b6c100ddc05e1b
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-04-06 14:50:34 +0300

    more py2 compat

--- baecc/instruments/__init__.py
+++ baecc/instruments/__init__.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from __future__ import absolute_import, division, print_function
 import gc
 import baecc
 from baecc.instruments.base import InstrumentData, PrecipMeasurer, datafilelistloop
